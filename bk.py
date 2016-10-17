97c9c22f6158ca4d359ce0be8f0a920866583f83
Pedro Baldera <pbaldera@ip-1-70-103-5.dob1.bcpc.bloomberg.com>
2016-10-17 16:24:11 -0400

    adding breakfast file

---
+++ bk.py
@@ -0,0 +1,21 @@
+breakfast_special = "Texas Omelet"
+breakfast_notes = "Contains brisket, horseradish cheddar"
+lunch_special = "Greek patty melt"
+lunch_notes = "like the regular one, but with tzatziki sauce"
+dinner_special = "Buffalo steak"
+dinner_notes = "Top loin with hot sauce and blue cheese.  NOT BUFFALO MEAT."
+
+
+print "Today's specials"
+print "*"*20
+print "Breakfast: ",
+print breakfast_special
+print breakfast_notes
+print
+print "Lunch: ",
+print lunch_special
+print lunch_notes
+print
+print "Dinner: ",
+print dinner_special
+print dinner_notes
